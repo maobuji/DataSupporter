691889c0aaa0c4382f34120190fb36de2012c9ad
maobuji <16770864@qq.com>
2018-05-23 18:56:24 +0800

    去除图片减少流量，以增加访问效率

--- housedata/src/main/java/www/lianjia/com/util/WebDrivereUtil.java
+++ housedata/src/main/java/www/lianjia/com/util/WebDrivereUtil.java
@@ -2,6 +2,9 @@ package www.lianjia.com.util;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
 
 /**
  * Created by Administrator on 2018/5/18.
@@ -12,7 +15,13 @@ public class WebDrivereUtil {
 
     public static synchronized WebDriver getWebDrivere() {
         System.setProperty("webdriver.chrome.driver", "tool-repository/selenium/chromedriver.exe");
-        return new ChromeDriver();
+
+        HashMap<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("profile.managed_default_content_settings.images", 2);
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setExperimentalOption("prefs", prefs);
+
+        return new ChromeDriver(chromeOptions);
     }
 
     public static synchronized void close(WebDriver driver) {
